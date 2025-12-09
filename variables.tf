5612ad42e0e506e4d80ac917f2427eecf4cab585
naresh <Naresh@gmail.com>
2025-12-09 14:11:30 +0530

    added main.tf code add lenght function

--- variables.tf
+++ variables.tf
@@ -33,7 +33,7 @@ variable "db_subnet" {
      type        = list(string)
      default = ["10.0.21.0/24","10.0.22.0/24"]
 }
-variable "availability_zone" {
+variable "availability_zones" {
      type        = list(string)
      default = ["us-east-1a","us-east-1b"]
 }
