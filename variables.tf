0b865212bab3ccc4baf47cfdcf7e1c8ab798b6b5
naresh <Naresh@gmail.com>
2026-01-18 22:22:21 +0530

    vpc module modification:tags

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "cidr_block" {
     type = string
-    default = "10.10.0.0/16"
+    
 }
 # variable "region" { }
 
@@ -30,15 +30,15 @@ variable "environment" {
 }
 variable "public_subnet" {
    type        = list(string)
-   default = ["10.0.1.0/24","10.0.2.0/24"]
+  
 }
 variable "private_subnet" {
     type        = list(string)
-    default = ["10.0.11.0/24","10.0.12.0/24"]
+  
 }
 variable "db_subnet" {
      type        = list(string)
-     default = ["10.0.21.0/24","10.0.22.0/24"]
+     
 }
 variable "availability_zones" {
      type        = list(string)
